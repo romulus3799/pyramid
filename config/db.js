5fc5db7a1b3b73868b4fc7a28af5571815beab80
Romil Havewala <romilrh@gmail.com>
2017-04-25 22:37:21 -0500

    Setup db, got empty server.js up and running

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url : 'mongodb://rhavewala:Prime2Echoes@pyramids-shard-00-00-hkv3l.mongodb.net:27017,pyramids-shard-00-01-hkv3l.mongodb.net:27017,pyramids-shard-00-02-hkv3l.mongodb.net:27017/pyramids?ssl=true&replicaSet=pyramids-shard-0&authSource=admin'
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+const bp = require('body-parser')
+const mo = require('method-override')
+const mongoose = require('mongoose');
+
+var db = require('./config/db')
+mongoose.connect(db.url)
+
+var port = process.env.port || 8080
+
+app.use(bp.json())
+app.use(bp.json({ type : 'application/vnd.api+json'}))
+app.use(bp.urlencoded({ extended : true}))
+app.use(mo('X-HTTP-Method-Override'))
+app.use(express.static(__dirname + '/public'))
+
+require('./app/routes')(app)
+app.listen(port)
+console.log('Tell me about port ' + port)
+
+exports = module.exports = app
