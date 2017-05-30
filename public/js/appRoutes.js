3b3677dfefc4aedc0c24b86f5ae6b97ec7df1cb7
Romil Havewala <romilrh@gmail.com>
2017-05-30 11:10:23 -0500

    broke paths to fi for gh-pages 3

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -5,11 +5,11 @@ angular.module('appRoutes', [])
 	$routeProvider
 		// TODO: add "/" page
 		.when('/pyramid/create', {
-			templateUrl		: 'views/editor.html',
+			templateUrl		: 'pyramid/public/views/editor.html',
 			controllerAs	: 'EditorController'
 		})
 		.when('/pyramid/pyramids', {
-			templateUrl		: 'views/pyramids.html',
+			templateUrl		: 'pyramid/public/views/pyramids.html',
 			controllerAs	: 'PyramidController'
 		})
 
